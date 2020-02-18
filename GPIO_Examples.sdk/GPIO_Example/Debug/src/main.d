3e0291feb33013aa7d0f656b79c71b8210165924
unknown <bkeceoglu@gmail.com>
2020-02-19 00:33:06 +0300

    first commit

---
+++ GPIO_Examples.sdk/GPIO_Example/Debug/src/main.d
@@ -0,0 +1,46 @@
+src/main.o src/main.o: ../src/main.cc \
+ ../../GPIO_Example_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../GPIO_Example_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../GPIO_Example_bsp/ps7_cortexa9_0/include/xgpio.h \
+ ../../GPIO_Example_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../GPIO_Example_bsp/ps7_cortexa9_0/include/xil_assert.h \
+ ../../GPIO_Example_bsp/ps7_cortexa9_0/include/xstatus.h \
+ ../../GPIO_Example_bsp/ps7_cortexa9_0/include/xgpio_l.h \
+ ../../GPIO_Example_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../GPIO_Example_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../GPIO_Example_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../GPIO_Example_bsp/ps7_cortexa9_0/include/bspconfig.h \
+ ../../GPIO_Example_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../GPIO_Example_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../GPIO_Example_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h \
+ ../../GPIO_Example_bsp/ps7_cortexa9_0/include/sleep.h
+
+../../GPIO_Example_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../GPIO_Example_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../GPIO_Example_bsp/ps7_cortexa9_0/include/xgpio.h:
+
+../../GPIO_Example_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../GPIO_Example_bsp/ps7_cortexa9_0/include/xil_assert.h:
+
+../../GPIO_Example_bsp/ps7_cortexa9_0/include/xstatus.h:
+
+../../GPIO_Example_bsp/ps7_cortexa9_0/include/xgpio_l.h:
+
+../../GPIO_Example_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../GPIO_Example_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../GPIO_Example_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../GPIO_Example_bsp/ps7_cortexa9_0/include/bspconfig.h:
+
+../../GPIO_Example_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../GPIO_Example_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../GPIO_Example_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
+
+../../GPIO_Example_bsp/ps7_cortexa9_0/include/sleep.h:
---
+++ GPIO_Examples.srcs/constrs_1/new/const.xdc
@@ -0,0 +1,12 @@
+set_property IOSTANDARD LVCMOS33 [get_ports {switch_tri_o[0]}]
+set_property IOSTANDARD LVCMOS33 [get_ports {led1_tri_o[1]}]
+set_property IOSTANDARD LVCMOS33 [get_ports {led1_tri_o[0]}]
+set_property PACKAGE_PIN G15 [get_ports {switch_tri_o[0]}]
+set_property PACKAGE_PIN M15 [get_ports {led1_tri_o[1]}]
+set_property PACKAGE_PIN M15 [get_ports {led1_tri_o[0]}]
+
+set_property IOSTANDARD LVCMOS33 [get_ports {switch_tri_i[0]}]
+set_property PACKAGE_PIN G15 [get_ports {switch_tri_i[0]}]
+
+set_property IOSTANDARD LVCMOS33 [get_ports {led0_tri_o[0]}]
+set_property PACKAGE_PIN M14 [get_ports {led0_tri_o[0]}]
